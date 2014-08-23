102c4a65aaad3cd3f4962dc3533539e0fd45c847
ials <ilizarazo@udistrital.edu.co>
2014-08-23 17:43:42 -0500

    R Programming Assignment 2
    This code comprises two functions that allow to cache the inverse of a
    matrix. It is assummed  that the matrix supplied is always invertible.

--- cachematrix.R
+++ cachematrix.R
@@ -1,13 +1,16 @@
 ## Programming Assignment 2: Lexical Scoping
 
-## This work is based on the  code from the R programming course  "Example: Caching the Mean of a Vector"
+## This work is based on code from the R programming course  "Example: Caching the Mean of a Vector"
 
-## As matrix inversion is a costly computation, it is advantageous to caching the inverse of a matrix rather than compute it again and again. 
+## As matrix inversion is a costly computation, it is advantageous to caching the inverse of a matrix rather than compute it 
+## again and again. 
 ## 
-## The following pair of functions cache the inverse of a matrix. 
+## The following pair of functions cache the inverse of a matrix.  
+## It is assumed that the matrix supplied is always invertible.
 
 
-## The function makeCacheMatrix creates a special "matrix" object that can cache its inverse. This function actually returns a list containing a function to: 
+## The function makeCacheMatrix creates a special "matrix" object that can cache its inverse. This function actually
+returns a list containing a function to: 
 ## 1-set the value of the matrix
 ## 2-get the value of the matrix
 ## 3-set the value of the inverse
